cb86d9311a811176c476d8bd5c60abc8e755ba66
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-08-12 15:31:49 -0300

    style reset and creation of all basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContactComponent } from './contact/contact.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactEditComponent } from './contact-edit/contact-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactComponent,
+    ContactListComponent,
+    ContactEditComponent
   ],
   imports: [
     BrowserModule,
